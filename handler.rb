eda7c5f97e82b046b5a62c2c7f6d734eaed1f242
Himanshu Gupta <cephalus.mail@gmail.com>
2019-03-29 12:55:05 +0530

    mute un-necessary functions

--- handler.rb
+++ handler.rb
@@ -6,11 +6,11 @@ require 'aws-sdk-lambda'  # v2: require 'aws-sdk'
 
 # lambda_boy = Aws::Lambda::Client.new(region: 'us-east-2')
 
-def hello(event:, context:)
-	puts event
-	# name1 = event["pathParameters"]["name"] : nil
-  { statusCode: 200, body: JSON.generate("Hello  ") }
-end
+# def hello(event:, context:)
+# 	puts event
+# 	# name1 = event["pathParameters"]["name"] : nil
+#   { statusCode: 200, body: JSON.generate("Hello  ") }
+# end
 
 def show_contents(event:, context:)
 	p Dir["/opt/**/*.*"]
@@ -30,16 +30,16 @@ def get_data(event:, context:)
 	# return data.css('.HeroContent').css('.HeroTextContent').text.strip
 end
 
-def up?(event:, context:)
-	foo
-	# Net::Ping::External.new(event).ping?
-end
+# def up?(event:, context:)
+# 	foo
+# 	# Net::Ping::External.new(event).ping?
+# end
 
-def data_scraper(url)
-	@crash = false
-	begin
-  	Nokogiri::HTML(open(url))
-	rescue 
-		@crash = true
-  end
+# def data_scraper(url)
+# 	@crash = false
+# 	begin
+#   	Nokogiri::HTML(open(url))
+# 	rescue 
+# 		@crash = true
+#   end
 end
